e38148dc94cdccafd6588e55d71058d5c3a9bbfa
Rodri.Dev <rodrigocn53@gmail.com>
2025-12-15 22:04:38 -0300

    cuarto-commit

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -7,16 +7,41 @@ const contact_data = [
         contact_unseen_messages: 5,
         last_message_content: 'testing',
         last_message_state: 'RECEIVED',/*'NOT_SEND' | 'NOT_RECEIVED' | 'RECEIVED' | 'SEEN'*/
-        last_message_created_at: new Date()
+        last_message_created_at: new Date(),
+        messages:[
+        {
+            message_id: 1,
+            message_content: 'Que tal?',
+            messages_state: 'SEEN',
+            messages_create_at: new Date(), 
+            send_by_me: true
+        },
+        {
+            message_id: 2,
+            message_content: 'Que tal?',
+            messages_state: 'SEEN',
+            messages_create_at: new Date(),
+            send_by_me: false 
+        }
+    ]
     },
     {
-        contact_id: 2,
-        contact_name: 'Juan',
-        contact_avatar: 'https://images.immediate.co.uk/production/volatile/sites/3/2017/12/yoda-the-empire-strikes-back-28a7558.jpg',
-        contact_unseen_messages: 0,
-        last_message_content: 'Que tal?',
-        last_message_state: 'SEEN',
-        last_message_created_at: new Date()
+    contact_id: 2,
+    contact_name: 'Juan',
+    contact_avatar: 'https://images.immediate.co.uk/production/volatile/sites/3/2017/12/yoda-the-empire-strikes-back-28a7558.jpg',
+    contact_unseen_messages: 0,
+    last_message_content: 'Que tal?',
+    last_message_state: 'SEEN',
+    last_message_created_at: new Date(),
+    messages:[
+        {
+        message_id: 1,
+        message_content: 'Que tal?',
+        messages_state: 'SEEN',
+        messages_create_at: new Date(), 
+        send_by_me: true
+        }
+    ]
     },
 ]
 
